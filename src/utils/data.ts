6714b5f086e2636ff9554364e24ab2ddbb5d2cc2
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-02-18 17:34:53 +0100

    Fix content

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,7 @@
 const services = [
     {
         title: "Bindung und Motivation",
-        description: "Qizify bindet Auszubildende ein, erweitert ihr Wissen, fördert die Loyalität und senkt gleichzeitig die Abbrecherquoten, wodurch die Investitionen des Unternehmens in die Ausbildung maximiert werden.",
+        description: "Learnslice bindet Auszubildende ein, erweitert ihr Wissen, fördert die Loyalität und senkt gleichzeitig die Abbrecherquoten, wodurch die Investitionen des Unternehmens in die Ausbildung maximiert werden.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
@@ -25,13 +25,13 @@ const services = [
     },
     {
         title: "Personalisiertes Lernen",
-        description: "Qizify passt Inhalte individuell an Ihre Auszubildenden an, wodurch das Verständnis und die Behaltensleistung gefördert werden, was wiederum zu besseren Prüfungsergebnissen und der Entwicklung eines kompetenten und kenntnisreichen Teams führt.",
+        description: "Learnslice passt Inhalte individuell an Ihre Auszubildenden an, wodurch das Verständnis und die Behaltensleistung gefördert werden, was wiederum zu besseren Prüfungsergebnissen und der Entwicklung eines kompetenten und kenntnisreichen Teams führt.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Umfassende Unterstützung",
-        description: "Qizify bietet umfassende Unterstützung für Mentoren, die alles vom Onboarding bis zur Prüfungsvorbereitung abdeckt und den Auszubildenden die notwendigen Fähigkeiten für ihr berufliches Wachstum in Ihrem Unternehmen vermittelt.",
+        description: "Learnslice bietet umfassende Unterstützung für Mentoren, die alles vom Onboarding bis zur Prüfungsvorbereitung abdeckt und den Auszubildenden die notwendigen Fähigkeiten für ihr berufliches Wachstum in Ihrem Unternehmen vermittelt.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
